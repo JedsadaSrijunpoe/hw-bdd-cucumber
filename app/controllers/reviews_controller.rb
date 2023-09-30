7493154f6d604680e13d076391b34837262eee9b
JedsadaSrijunpoe <tae456852@gmail.com>
2023-09-30 07:11:35 +0000

    Change review_params to private

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -41,6 +41,7 @@ class ReviewsController < ApplicationController
         redirect_to movie_path(@movie)
     end
 
+    private
     def review_params
         params.require(:review).permit(:potatoes, :comments, :moviegoer, :movie)
     end
